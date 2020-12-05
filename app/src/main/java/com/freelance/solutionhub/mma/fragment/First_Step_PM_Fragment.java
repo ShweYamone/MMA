242cdc5a1333e2409fef194ec3d57961fe858f24
ShweYamoneOO <shweyamoneoo1234@gmail.com>
2020-12-05 19:29:20 +0630

    commit

--- app/src/main/java/com/freelance/solutionhub/mma/fragment/First_Step_PM_Fragment.java
+++ app/src/main/java/com/freelance/solutionhub/mma/fragment/First_Step_PM_Fragment.java
@@ -23,6 +23,6 @@ public class First_Step_PM_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first__step__p_m_, container, false);
+        return inflater.inflate(R.layout.fragment_first_step_p_m_, container, false);
     }
 }
