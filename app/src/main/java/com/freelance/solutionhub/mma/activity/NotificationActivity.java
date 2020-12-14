6826db20f3c234c0f911668109894b187cdaf206
Aung Kyaw Mon <aungkyawmon.am@gmail.com>
2020-12-14 19:45:03 +0630

    h

--- app/src/main/java/com/freelance/solutionhub/mma/activity/NotificationActivity.java
+++ app/src/main/java/com/freelance/solutionhub/mma/activity/NotificationActivity.java
@@ -18,11 +18,7 @@ import com.freelance.solutionhub.mma.model.NotificationModel;
 import com.freelance.solutionhub.mma.util.SharePreferenceHelper;
 import com.freelance.solutionhub.mma.util.WebSocketUtils;
 
-
-import java.io.IOException;
-import java.net.Socket;
-import java.net.URI;
-import java.nio.channels.Channel;
+import org.phoenixframework.channels.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,6 +39,7 @@ public class NotificationActivity extends AppCompatActivity {
     private Socket socket;
     private Channel channel;
     private WebSocketUtils webSocketUtils;
+
     private SharePreferenceHelper mSharedPreference;
     private String urlStr, channelStr;
 
@@ -64,42 +61,38 @@ public class NotificationActivity extends AppCompatActivity {
 
 
         /************WebScoket***************/
-/*
-        final WebSocketUtils utils = new WebSocketUtils(getApplicationContext());
-        final String url = utils.getUrl();
-        final String topic = utils.getTopic();
+
+        Uri.Builder url = Uri.parse( "ws://hub-nightly-public-alb-1826126491.ap-southeast-1.elb.amazonaws.com/socket/websocket" ).buildUpon();
+        //   url.appendQueryParameter("vsn", "2.0.0");
+        url.appendQueryParameter( "token", "eyJhbGciOiJSUzI1NiJ9.eyJyYW5kVXVpZCI6IjI0ZWU1MzE3ZWE0ZjQ3MmU4OGJmNzViMTkxNGFjYzZiIiwiYXVkIjpbImFwcC11c2VyIl0sImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfbmFtZSI6Im1vYmlsZS0xIiwicGVybWlzc2lvbnMiOlsiRlVOQ19DUlVEX01NTV9DT05URU5UX19DT05UUkFDVE9SX1RBU0tTIiwiRlVOQ19WSUVXX01NTV9DT05URU5UX0NPTlRSQUNUT1JfVEFTS1MiXSwic2NvcGUiOlsiYXBwbGljYXRpb24iXSwiYXRpIjoiMmM0ODY1MWYtNTAyYi00N2E0LTlhOWMtMDc2MDE1OTE4MTQ2IiwiZXhwIjoxNjA3OTQ4OTYyLCJhdXRob3JpdGllcyI6WyJDT05UUkFDVE9SX01NTV9PRkZJQ0VSIiwiQ09OVFJBQ1RPUl9NTU1fVklFV0VSIl0sImp0aSI6ImJhNmMyN2VlLTZlNGQtNGUzNy04MWZjLWNiODNmM2U5ZmJmNiIsImNsaWVudF9pZCI6InRydXN0ZWQtYXBwIiwidXNlcm5hbWUiOiJtb2JpbGUtMSJ9.O7X8IWUi4PW23yXHzNOgErdAgQP_DxXYQSCQSR4o_F-DoFNH9UbGogyuqwpDkGvKE0SmZr5snZzar0mlTI6xZ2ynAueFJ7CUtUrk2NW8hyqjEJPQEo3cCaben2Oe1it5x8Cbb0QK3VbqccCDnUiUb1P7K5Vh6V3woYIyfivu1cKZUI2juhjnoahbqHSpfknVCrGHdgiqLdxYeG6T4YnXWxqbtSe6Ll0F5WJAg91pFNySkREdrPQpgNO0EXwThB4PG3B4bynuthb4MdAmoBq0b5ZkJ5elL6jNNQNsbhRwKzcO8illbtJMAy5vglvCuXJAzC_Xqgy_2tV4DkBBIKbtQw");
         try {
-            socket = new Socket(
-                    "hub-nightly-public-alb-1826126491.ap-southeast-1.elb.amazonaws.com/socket/websocket?vsn=2.0.0&token="
-                                + mSharedPreference.getToken()
-            );
+            //    Log.i("Websocket", url.toString());
+            socket = new Socket(url.build().toString());
             socket.connect();
 
-            socket.onOpen(new ISocketOpenCallback() {
-                @Override
-                public void onOpen() {
-                    Toast.makeText(getApplicationContext(), "connected" ,Toast.LENGTH_SHORT).show();
-                    channel = socket.chan(topic, null);
-                    try {
-                        channel.join().receive("ok", new IMessageCallback() {
-                            @Override
-                            public void onMessage(Envelope envelope) {
-                                Log.i("WEBSOCKET", "onMessage: " + "Joined Notification Successfully");
-                            }
-                        });
-                    } catch (Exception e) {
-                        Log.e("WEBSOCKET", "Failed to join channel " + topic, e);
-                     //   handleTerminalError(e);
-                    }
+            channel = socket.chan("notification", null);
 
-                }
-            });
-
+            channel.join()
+                    .receive("ok", new IMessageCallback() {
+                        @Override
+                        public void onMessage(Envelope envelope) {
+                            Log.i("Websocket", "Joined with " + envelope.toString());
+                        }
+                    })
+                    .receive("error", new IMessageCallback() {
+                        @Override
+                        public void onMessage(Envelope envelope) {
+                            Log.i("Websocket", "NOT Joined with ");
+                        }
+                    })
+            ;
         } catch (Exception e) {
-            Log.e(TAG, "Failed to connect", e);
-          //  handleTerminalError(e);
+            Toast.makeText(getApplicationContext(), "Exception" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Websocket",  e.getMessage() + "\n" + e.getLocalizedMessage());
         }
-*/
+        Toast.makeText(getApplicationContext(), "END", Toast.LENGTH_SHORT).show();
+        Log.i("Websocket",  "END");
+
     }
 
     private void setupToolbar() {
