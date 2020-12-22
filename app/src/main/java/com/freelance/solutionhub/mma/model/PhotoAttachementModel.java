5cebd546333acbd76552d31daf20d56544a7a47e
Aung Kyaw Mon <aungkyawmon.am@gmail.com>
2020-12-22 15:51:44 +0630

    Notification Completed

---
+++ app/src/main/java/com/freelance/solutionhub/mma/model/PhotoAttachementModel.java
@@ -0,0 +1,24 @@
+package com.freelance.solutionhub.mma.model;
+
+import java.util.List;
+
+public class PhotoAttachementModel {
+    public List<PreMaintenance> preMaintenance;
+    public List<PostMaintenance> postMaintenance;
+
+    public List<PreMaintenance> getPreMaintenance() {
+        return preMaintenance;
+    }
+
+    public void setPreMaintenance(List<PreMaintenance> preMaintenance) {
+        this.preMaintenance = preMaintenance;
+    }
+
+    public List<PostMaintenance> getPostMaintenance() {
+        return postMaintenance;
+    }
+
+    public void setPostMaintenance(List<PostMaintenance> postMaintenance) {
+        this.postMaintenance = postMaintenance;
+    }
+}
