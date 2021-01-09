5183aa048d32b84c696317516013d84dcf41422b
ShweYamoneOO <shweyamoneoo1234@gmail.com>
2021-01-09 14:00:01 +0630

    commit

--- app/src/main/java/com/digisoft/mma/util/ForegroundService.java
+++ app/src/main/java/com/digisoft/mma/util/ForegroundService.java
@@ -163,7 +163,7 @@ public class ForegroundService extends Service {
     public void sendNotification(String title, String type) {
 
         if (type.length() > 95) {
-            type = type.substring(0, 94) + "read more...";
+            type = type.substring(0, 90) + " ... ";
         }
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANNEL_ID = "tutorialspoint_01";
